e5801758c43c7b00c5b1c224c57306b352f07802
Nikhil Gurnani <gurnanikhil@gmail.com>
2017-06-03 14:25:53 +0530

    I think I fucked up.

--- src/app/services/user-presence.service.ts
+++ src/app/services/user-presence.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Observable } from 'rxjs/Observable';
+import { FirebaseService }  from './firebase.service';
 import * as firebase from 'firebase/app';
 import { FlashMessagesService } from 'angular2-flash-messages';
 import { Router } from '@angular/router';
@@ -11,9 +12,24 @@ export class UserPresenceService {
 	user: Observable<firebase.User>;
 	User:any;
 	constructor(public afAuth:AngularFireAuth,
-			private router:Router,
-			private flash: FlashMessagesService){
+		private router:Router,
+		private firebaseService: FirebaseService,
+		private flash: FlashMessagesService)
+	{
 		this.user = afAuth.authState;
+		this.afAuth.authState.subscribe(user => {
+			if (user != null)
+			{
+				if(firebaseService.checkUser(user.uid) == null)
+				{
+					
+				}
+			}
+			else if(user == null)
+			{
+				console.log("No user logged in");
+			}
+		});
 	}
 
 	loginWithGoogle()
